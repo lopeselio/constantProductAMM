7959fb22f3a46522a6df7742c317b1606769473d
devELIOper <lopeselio@gmail.com>
2022-07-28 11:19:12 -0700

    init state variables

--- CPAmm.sol
+++ CPAmm.sol
@@ -1,1 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
 
+import "./IERC20.sol";
+
+contract CPAMM {
+    IERC20 public immutable token0;
+    IERC20 public immutable token1;
+
+    uint public reserve0;
+    uint public reserve1;
+
+    uint public totalSupply;
+    mapping(address => uint) public balanceOf;
+
+    constructor(address _token0, address _token1) {
+        token0 = IERC20(_token0);
+        token1 = IERC20(_token1)
+    }
+
+    function _mint(address _to, uint _amount) private {
+        balanceOf[_to] += _amount;
+        totalSupply += _amount;
+    }
+
+    function _burn(address _from, uint _amount) private {
+        balanceOf[_from] -= _amount;
+        totalSupply -= _amount;
+    }
+}
